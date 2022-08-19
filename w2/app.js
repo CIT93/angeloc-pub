c1b6dc6833904c6549fd0edf10c381c7d02da862
Angelo0121 <Angelo01castaneda@gmail.com>
2022-08-19 06:04:58 +0000

    week 2 learnTogether

--- w2/app.js
+++ w2/app.js
@@ -5,15 +5,18 @@ let outputDiv = document.getElementedById("output")
 outputDiv.append(newParagrapgh)
 
 }
-
+//usually wake up around 8 or 9am
 let currentTime = 9
+//work starts at 11 am til 7
 let workStartTime = 11
+//time before work is usually an hour or maybe two
 let availableTime = 1
 let timeBeforeWork = workStartTime - currentTime
+//time to drive my car
 let myTimeToDrive = true
 let TimeToEatBefore = true
 
-showOnPage("<b>this output is based on the following global variable data:<b>")
+showOnPage("this output is based on the following global variable data")
 showOnPage("current Time --->" + currentTime)
 showOnPage("Work Time --->" + workStartTime)
 showOnPage("availableTime --->" + availableTime)
