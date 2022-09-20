5d8f3293ae78ef7edb4a72e3173d7add66e900ec
Angelo0121 <Angelo01castaneda@gmail.com>
2022-09-20 02:25:02 +0000

    video 50 code

--- notes-app/note-app.js
+++ notes-app/note-app.js
@@ -1,1 +1,18 @@
-console.log("this is a different file")
+
+//DOM - Document object model
+
+//query and remove
+//const p = document.querySelector("p")
+//p.remove()
+
+
+//query all and remove
+
+const ps = document.querySelectorAll("p")
+
+
+ps.forEach(function (p) {
+ p.textContent = "*****"
+    //console.log(p.textContent)
+    //p.remove
+})
