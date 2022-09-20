79dcffbcca9dddd79afd0c6be454d4043b76d72d
Angelo0121 <Angelo01castaneda@gmail.com>
2022-09-20 03:02:00 +0000

    video 52 my code

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,7 +1,34 @@
-const paragraphs = document.querySelectorAll("p")
+const todos = [{
+    text: "take out the trash",
+    completed: true
+}, {
+        text: "make my bed",
+        completed: false
+    }, {
+        text: "brush my teeth",
+        completed: true
+    }, {
+        text: "feed my dogs",
+        completed: false
+    }, {
+        text: "do laundry",
+        completed: true
+    }]
 
-paragraphs.forEach(function(paragraphs) {
-if (paragraph.textContent.includes("the")) {
-    paragraph.remove()
-}
+    const incompleteToDos = todos.filter(function (todo) {
+        return !todo.completed
+    })
+
+const summary = document.createElement("h2")
+summary.textContent = `you have ${incompleteToDos.length} todos left`
+document.querySelector("body").appendChild(summary)
+    
+todos.forEach(function (todo) {
+    const p = document.createElement("p")
+    p.textContent = todo.text
+    document.querySelector("body").appendChild(p)
 })
+
+
+//you have 2 todos left (p element)
+//add a p for each todo above
