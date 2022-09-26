7c8057c7bfb48bda02810dd1d67d461db214e27b
Angelo0121 <Angelo01castaneda@gmail.com>
2022-09-26 19:09:06 +0000

    video 57 my code

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,19 +15,8 @@ const todos = [{
         completed: true
     }]
 
-    const incompleteToDos = todos.filter(function (todo) {
-        return !todo.completed
-    })
+   
 
-const summary = document.createElement("h2")
-summary.textContent = `you have ${incompleteToDos.length} todos left`
-document.querySelector("body").appendChild(summary)
-    
-todos.forEach(function (todo) {
-    const p = document.createElement("p")
-    p.textContent = todo.text
-    document.querySelector("body").appendChild(p)
-})
 
 
 document.querySelector("#add-todo").addEventListener("click", function (e) {
@@ -36,3 +25,40 @@ console.log("add new todo")
 document.querySelector("#todo-text").addEventListener("input", function (e) {
     console.log(e.target.value)
 })
+
+    const filters = {
+        searchText: ""
+    }
+
+    const rendertodo = function (todo, filters) {
+        const filterTodos = todos.filter(function (todo) {
+            return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        })
+        const incompleteToDos = filterTodos.filter(function (todo) {
+            return !todo.completed});
+
+        document.querySelector("#todos").innerHTML= ""
+
+        const summary = document.createElement("h2")
+        summary.textContent = `you have ${incompleteToDos.length} todos left`
+        document.querySelector("#todos").appendChild(summary)
+            
+        filterTodos.forEach(function (todo) {
+            const p = document.createElement("p")
+            p.textContent = todo.text
+            document.querySelector("#todos").appendChild(p)
+        })
+
+    }
+
+document.querySelector("#search-Text").addEventListener("input", function (e) {
+    filters.searchText = e.target.value
+    rendertodo(todos, filters)
+})
+
+    rendertodo(todos, filters)
+
+
+//1.setup a div contain for todos
+//2.setup filters(search text)
+//3.create rendertodo function
